b1e110d4ca60b50aad67551f0ca8b0346f357e34
Yue Liu <your_name@domain.com>
2016-08-06 23:15:45 -0700

    message to describe changes

--- cachematrix.R
+++ cachematrix.R
@@ -4,11 +4,6 @@
 ## Write a short comment describing this function
 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
-
-
 makeCacheMatrix <- function(x = matrix()){
         inv <- NULL
         set <- function(y){
